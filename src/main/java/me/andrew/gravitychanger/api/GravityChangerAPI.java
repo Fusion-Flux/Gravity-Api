2ea3f36c8aac69c9424c6cbc2b97c0ac797766ee
Gaider10 <72764892+Gaider10@users.noreply.github.com>
2021-12-15 19:23:25 +0200

    Add GravityChangerAPI#getAppliedGravityDirection() and more info about gravity directions

--- src/main/java/me/andrew/gravitychanger/api/GravityChangerAPI.java
+++ src/main/java/me/andrew/gravitychanger/api/GravityChangerAPI.java
@@ -9,16 +9,28 @@ import net.minecraft.util.math.Vec3d;
 
 public abstract class GravityChangerAPI {
     /**
-     * Returns gravity direction for the given player
+     * Returns the applied gravity direction for the given player
+     * This is the direction that directly affects everything this mod changes
+     * If the player is riding a vehicle this will be the applied gravity direction of the vehicle
+     * Otherwise it will be the main gravity gravity direction of the player itself
+     */
+    public static Direction getAppliedGravityDirection(PlayerEntity playerEntity) {
+        return ((EntityAccessor) playerEntity).gravitychanger$getAppliedGravityDirection();
+    }
+
+    /**
+     * Returns the main gravity direction for the given player
+     * This may not be the applied gravity direction for the player, see GravityChangerAPI#getAppliedGravityDirection
      */
     public static Direction getGravityDirection(PlayerEntity playerEntity) {
         return ((RotatableEntityAccessor) playerEntity).gravitychanger$getGravityDirection();
     }
 
     /**
-     * Sets gravity direction for the given player
+     * Sets the main gravity direction for the given player
      * If the player is a ServerPlayerEntity and gravity direction changed also syncs the direction to the clients
      * If the player is either a ServerPlayerEntity or a ClientPlayerEntity also slightly adjusts player position
+     * This may not immediately change the applied gravity direction for the player, see GravityChangerAPI#getAppliedGravityDirection
      */
     public static void setGravityDirection(PlayerEntity playerEntity, Direction gravityDirection) {
         ((RotatableEntityAccessor) playerEntity).gravitychanger$setGravityDirection(gravityDirection, false);
