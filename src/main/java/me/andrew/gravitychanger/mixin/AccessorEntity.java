56c6ae9d14682f7de9c6bd7756c44c7ef70dac77
Fusion-Flux <71662169+Fusion-Flux@users.noreply.github.com>
2022-04-28 15:06:52 -0500

    CCA Implementation

---
+++ src/main/java/me/andrew/gravitychanger/mixin/AccessorEntity.java
@@ -0,0 +1,12 @@
+package me.andrew.gravitychanger.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Box;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Entity.class)
+public interface AccessorEntity {
+    @Invoker("calculateBoundingBox")
+    Box gravity$calculateBoundingBox();
+}
