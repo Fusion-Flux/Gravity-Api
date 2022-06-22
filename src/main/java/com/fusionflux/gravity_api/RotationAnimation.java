af19ca7da45cb1ebe96fdff3bde4ed6f7379bb00
qouteall <qouteall@163.com>
2022-06-22 12:45:14 +0800

    Fix wrong frustum culling during rotation animation

--- src/main/java/com/fusionflux/gravity_api/RotationAnimation.java
+++ src/main/java/com/fusionflux/gravity_api/RotationAnimation.java
@@ -1,10 +1,14 @@
 package com.fusionflux.gravity_api;
 
 import com.fusionflux.gravity_api.util.RotationUtil;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Quaternion;
 
+@Environment(EnvType.CLIENT)
 public class RotationAnimation {
     private static boolean inAnimation = false;
     private static Quaternion startGravityRotation;
@@ -48,6 +52,9 @@ public class RotationAnimation {
         }
         
         double delta = ((double) (now - startTimeMs)) / (endTimeMs - startTimeMs);
+    
+        // make sure that frustum culling updates when running rotation animation
+        MinecraftClient.getInstance().worldRenderer.scheduleTerrainUpdate();
         
         return RotationUtil.interpolate(
             startGravityRotation, endGravityRotation,
