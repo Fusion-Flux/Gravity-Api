3e20f480bfa40a9a5301193bc0dbc5945fe06b2b
FinlaySML <2100719@uad.ac.uk>
2022-07-03 12:44:55 +0100

    Optimise imports

--- src/main/java/com/fusionflux/gravity_api/util/NetworkUtil.java
+++ src/main/java/com/fusionflux/gravity_api/util/NetworkUtil.java
@@ -2,9 +2,6 @@ package com.fusionflux.gravity_api.util;
 
 import com.fusionflux.gravity_api.api.GravityChangerAPI;
 import com.fusionflux.gravity_api.api.RotationParameters;
-import com.fusionflux.gravity_api.util.packet.DefaultGravityPacket;
-import com.fusionflux.gravity_api.util.packet.InvertGravityPacket;
-import com.fusionflux.gravity_api.util.packet.OverwriteGravityPacket;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.MinecraftClient;
--- src/main/java/com/fusionflux/gravity_api/util/packet/GravityPacket.java
+++ src/main/java/com/fusionflux/gravity_api/util/packet/GravityPacket.java
@@ -2,7 +2,6 @@ package com.fusionflux.gravity_api.util.packet;
 
 import com.fusionflux.gravity_api.util.GravityComponent;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.server.network.ServerPlayerEntity;
 
 public abstract class GravityPacket {
     public abstract void write(PacketByteBuf buf);
